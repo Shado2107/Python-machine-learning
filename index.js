2077cc70defca990d329488ec4c13fa8fc2a88d6
Shado2107 <romalmeida21@gmail.com>
2024-02-22 01:51:13 +0000

    add auto update readme

---
+++ index.js
@@ -0,0 +1,81 @@
+const { promises: fs } = require('fs');
+const readme = require('./readme');
+
+const msInOneDay = 1000 * 60 * 60 * 24;
+
+const today = new Date();
+
+function generateNewREADME() {
+  const readmeRow = readme.split('\n');
+
+  function updateIdentifier(identifier, replaceText) {
+    const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
+    readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
+      `<#${identifier}>`,
+      replaceText
+    );
+  }
+
+  const identifierToUpdate = {
+    day_before_new_years: getDBNWSentence(),
+    today_date: getTodayDate(),
+    gabot_signing: getGabotSigning(),
+  };
+
+  Object.entries(identifierToUpdate).forEach(([key, value]) => {
+    updateIdentifier(key, value);
+  });
+
+  return readmeRow.join('\n');
+}
+
+const moodByDay = {
+  1: 'excitement',
+  2: 'tranquility',
+  3: 'joy',
+  4: 'curiosity',
+  5: 'serenity',
+  6: 'adventure',
+  7: 'gratitude',
+};
+
+function getGabotSigning() {
+  const mood = moodByDay[today.getDay() + 1];
+  return `🤖 This README.md is updated with ${mood}, by The Bot ❤️`;
+}
+
+function getTodayDate() {
+  return today.toDateString();
+}
+
+function getMySelf() {
+  // test if we are in a PAIR DAY
+  return today.getDate() % 2 === 0
+    ? Math.floor(Math.random() * 2)
+      ? 'penguin 🐧'
+      : 'bear 🐻'
+    : 'penguin bear 🐧🐻';
+}
+
+function getDBNWSentence() {
+  const nextYear = today.getFullYear() + 1;
+  const nextYearDate = new Date(String(nextYear));
+
+  const timeUntilNewYear = nextYearDate.getTime() - today.getTime();
+  const dayUntilNewYear = Math.round(timeUntilNewYear / msInOneDay);
+
+  return `**${dayUntilNewYear} day before ${nextYear} ⏱**`;
+}
+
+const findIdentifierIndex = (rows, identifier) =>
+  rows.findIndex((r) => Boolean(r.match(new RegExp(`<#${identifier}>`, 'i'))));
+
+const updateREADMEFile = (text) => fs.writeFile('./README.md', text);
+
+function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADMEFile(newREADME);
+}
+main();
---
+++ readme.js
@@ -0,0 +1,17 @@
+module.export = ` 
+
+
+Bienvenue dans le référentiel dédié à l'apprentissage personnel 
+de Pandas ! Ce référentiel vise à fournir une ressource complète pour 
+quiconque souhaite maîtriser l'utilisation de la bibliothèque Pandas en 
+Python pour la manipulation et l'analyse de données.
+
+
+
+
+Credits: [Shado2107](https://github.com/Shado2107)
+
+<#gabot_signing>
+
+Last Edited on: <#today_date>
+` 
